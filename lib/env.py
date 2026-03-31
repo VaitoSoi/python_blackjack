f592aba88a37d299e1381093a1a0de477a7be9f4
VaitoSoi <duyduy15456@gmail.com>
2026-03-31 14:26:20 +0700

    feat: add env loader

---
+++ lib/env.py
@@ -0,0 +1,13 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def require_env(var: str):
+    val = getenv(var)
+    if val is None:
+        raise RuntimeError(f"{var} is required but not found")
+    return val
+
+TOKEN = require_env("TOKEN")
